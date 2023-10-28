4d2cecc1875cfb324b328de732e846a7672b8288
DevRam <ramsheed.rahim90@gmail.com>
2023-10-28 13:53:41 +0100

    initial commit

---
+++ src/main/java/com/ramapps/dricareresearch/DriCareresearchApplication.java
@@ -0,0 +1,13 @@
+package com.ramapps.dricareresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriCareresearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriCareresearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramapps/dricareresearch/DriCareresearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramapps.dricareresearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriCareresearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
